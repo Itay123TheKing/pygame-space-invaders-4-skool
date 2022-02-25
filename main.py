5a3f6fd4dbb9f92c511981ed13a5ebb963705526
Itay Czaczkes <itayczaczkes@gmail.com>
2022-02-25 19:35:21 +0200

    tweaked stars

--- main.py
+++ main.py
@@ -15,11 +15,14 @@ def hsv2rgb(h, s=1.0, v=1):
 	return tuple(int(i * 255) for i in colorsys.hsv_to_rgb(h, s, v))
 
 def main():
-	screen = pygame.display.set_mode((WIDTH, HEIGHT))
-	alphaSurf = pygame.Surface(screen.get_size(), pygame.SRCALPHA)
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	# main screen
+	alphaSurf = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.SRCALPHA)
+	# surface for adding alpha visual effect
+	
 	clock = pygame.time.Clock()
 	allSprites = pygame.sprite.Group()
-	player = Player(HALFWIDTH, HEIGHT - 10, allSprites)
+	player = Player(SCREEN_HALF_WIDTH, SCREEN_HEIGHT - 10, allSprites)
 	background = Background(screen)
 
 	h = 0
@@ -32,13 +35,12 @@ def main():
 
 		dt = clock.tick(FPS) / 1000.0
 		keys = pygame.key.get_pressed()
-		screen.fill(BLACK)
 		
-		if keys[ESCAPE]: return
-		if keys[LEFT]:
-			player.update(LEFT, dt)
-		elif keys[RIGHT]:
-			player.update(RIGHT, dt)
+		if keys[pygame.K_ESCAPE]: return
+		if keys[K_LEFT]:
+			player.update(K_LEFT, dt)
+		elif keys[K_RIGHT]:
+			player.update(K_RIGHT, dt)
 		else:
 			player.update(None, dt)
 
--- player.py
+++ player.py
@@ -6,7 +6,7 @@ class Player(pygame.sprite.Sprite):
 	def __init__(self, x, y, *groups):
 		super().__init__(*groups)
 		self.image = pygame.Surface((32, 32))
-		self.image.fill(WHITE)
+		self.image.fill(C_WHITE)
 		self.rect = self.image.get_rect()  # Get rect of some size as 'image'.
 		self.rect.bottom = y
 		self.rect.centerx = x
@@ -14,24 +14,24 @@ class Player(pygame.sprite.Sprite):
 		
 
 	def update(self, dir, dt):
-		if dir == LEFT:
-			self.velocity -= SPEEDUP * dt
-		elif dir == RIGHT:
-			self.velocity += SPEEDUP * dt
+		if dir == K_LEFT:
+			self.velocity -= PLAYER_SPEEDUP * dt
+		elif dir == K_RIGHT:
+			self.velocity += PLAYER_SPEEDUP * dt
 		else:
-			self.velocity += -sign(self.velocity) * SLOWDOWN * dt
-			if abs(self.velocity) < SLOWDOWN * dt:
+			self.velocity += -sign(self.velocity) * PLAYER_SLOWDOWN * dt
+			if abs(self.velocity) < PLAYER_SLOWDOWN * dt:
 				self.velocity = 0
 	
-		if self.velocity > MAX_VELOCITY:
-			self.velocity = MAX_VELOCITY
-		if self.velocity < -MAX_VELOCITY:
-			self.velocity = -MAX_VELOCITY
+		if self.velocity > PLAYER_MAX_VELOCITY:
+			self.velocity = PLAYER_MAX_VELOCITY
+		if self.velocity < -PLAYER_MAX_VELOCITY:
+			self.velocity = -PLAYER_MAX_VELOCITY
 		
 		self.rect.move_ip(self.velocity, 0)
 		if self.rect.left < 0:
 			self.rect.left = 0
 			self.velocity = 0
-		if self.rect.right > WIDTH:
-			self.rect.right = WIDTH
+		if self.rect.right > SCREEN_WIDTH:
+			self.rect.right = SCREEN_WIDTH
 			self.velocity = 0
