36541a7cb419cd1fddddb1fa1194e920f51f50be
Itay Czaczkes <itayczaczkes@gmail.com>
2022-03-02 11:53:50 +0200

    commit for push

--- alpha_layer.py
+++ alpha_layer.py
@@ -1,47 +1,80 @@
-from ctypes import Union
-from typing import Literal, List
+from typing import Literal, Tuple, Union, List
+from enum import Enum
+from numpy import isin
 import pygame
 import colorsys
+
+class flags(Enum):
+	NONE            = 0
+	NO_SOLID_COLOUR = 1 << 0
+	HUE_SHIFT       = 1 << 1
 
 class AlphaLayer:
 	def __init__(self, output: pygame.Surface) -> None:
 		self.output = output
 		self.screen = pygame.Surface(output.get_size(), pygame.SRCALPHA)
-		self.binded: List[self._Binded] = []
+		self.binded: List[self.__Binded] = []
 
-	def render(self, surface: pygame.Surface, pos: Tuple[int, int], solidColour: Union[pygame._common._ColorValue, Literal[False]] = (0, 0, 0)) -> None:
-		if solidColour:
-			self.screen.blit(pygame.mask.from_surface(surface).to_surface(setcolor=solidColour), (x, y))
+	def render(self, surface: pygame.Surface, pos: Tuple[int, int], solid_colour) -> None:
+		if solid_colour:
+			solid = pygame.mask.from_surface(surface).to_surface(setcolor=solid_colour)
+			solid.convert_alpha()
+			solid.set_colorkey((0, 0, 0))
+			self.screen.blit(solid, pos)
 		else:
-			self.screen.blit(b.img(), b.pos())
-
+			self.screen.blit(surface, pos)
 	 
-	def render_all_binded(self):
-		self.screen.fill((255, 255, 255, 220), special_flags=pygame.BLEND_RGBA_MULT)
+	def render_all_binded(self) -> None:
 		for b in self.binded:
-			if b.rotateHue:
+			if b.hue_shift:
+				b.update_hue()
+			if b.no_solid_colour:
+				self.render(b.img(), b.pos(), False)
+			else:
+				self.render(b.img(), b.pos(), b.colour)
+			
+		self.screen.fill((255, 255, 255, 220), special_flags=pygame.BLEND_RGBA_MULT)
 
+	def draw(self) -> None:
+		self.output.blit(self.screen, (0, 0))
 
-	
-	@staticmethod
-	def __hue_to_rgb(h):
-		return tuple(int(i * 255) for i in colorsys.hsv_to_rgb(h / 255, 1, 1))
+	def bind(self, spr: Union[pygame.sprite.Sprite, pygame.sprite.Group], colour, special_flags: flags = flags.NONE) -> None:
+		if isinstance(spr, pygame.sprite.Sprite):
+			if special_flags.value & flags.NO_SOLID_COLOUR.value:
+				self.binded.append(self.__Binded(spr, colour, False, True))
+			elif special_flags.value & flags.HUE_SHIFT.value:
+				self.binded.append(self.__Binded(spr, colour, True, False))
+			else:
+				self.binded.append(self.__Binded(spr, colour, False, False))
+		elif isinstance(spr, pygame.sprite.Group):
+			for s in spr:
+				if special_flags.value & flags.NO_SOLID_COLOUR.value:
+					self.binded.append(self.__Binded(s, colour, False, True))
+				elif special_flags.value & flags.HUE_SHIFT.value:
+					self.binded.append(self.__Binded(s, colour, True, False))
+				else:
+					self.binded.append(self.__Binded(s, colour, False, False))
+		else:
+			raise TypeError("spr must be a pygame.sprite.Sprite or pygame.sprite.Group")
 
 	class __Binded:
-		def __init__(self, obj: any, colour: pygame._common._ColorValue, rotateHue: bool = False, useOriginalColour: bool = False) -> None:
-			self.obj = obj
+		def __init__(self, spr: pygame.sprite.Sprite, colour, hue_shift: bool = False, no_solid_colour: bool = False) -> None:
+			self.spr = spr
 			self.colour = colour
-			self.rotateHue = rotateHue
-			self.useOriginalColour = useOriginalColour
+			self.hue_shift = hue_shift
+			self.no_solid_colour = no_solid_colour
 			self.hue, self.saturation, self.value = colorsys.rgb_to_hsv(*(i / 255 for i in self.colour))
+			self.hue = (self.hue * 255) // 1 
+			
+		def pos(self) -> None:
+			return self.spr.rect.topleft
 		
-		def pos(self):
-			return self.rect.topleft
-		
-		def img(self):
-			return self.obj.image
+		def img(self) -> None:
+			return self.spr.image
 
-		def update_hue(self):
-			self.hue += 1
-			if self.hue > 255:
-				self.hue = 0
+		def update_hue(self) -> None:
+			if self.hue_shift:
+				self.hue += 1
+				if self.hue > 255:
+					self.hue = 0
+				self.colour = (i * 255 for i in colorsys.hsv_to_rgb(self.hue / 255, self.saturation, self.value))
--- enemy.py
+++ enemy.py
@@ -3,6 +3,7 @@ from typing import Literal
 from constants import *
 import pygame
 import animated_sprite
+import random
 
 class EnemyType(Enum):
 	SQUID   = SMALL  = TYPE1 = 1
@@ -18,6 +19,32 @@ class Enemy(animated_sprite.AnimatedSprite):
 		self.type = type
 		self.x = x
 		self.y = y
+		self.dirx = random.choice([-1, 1])
+		self.diry = random.choice([-1, 1])
 
 	def draw(self, surface: pygame.Surface) -> None:
 		surface.blit(self.image, (self.x, self.y))
+	
+	def update(self) -> None:
+		super().update()
+		self.x += ENEMY_SPEED * self.dirx
+		self.y += ENEMY_SPEED * self.diry
+		self.rect.x = self.x
+		self.rect.y = self.y
+		if self.rect.top < 0:
+			self.rect.top = 0
+			self.y = self.rect.y
+			self.diry = -self.diry
+		if self.rect.bottom > SCREEN_HEIGHT:
+			self.rect.bottom = SCREEN_HEIGHT
+			self.y = self.rect.y
+			self.diry = -self.diry
+		if self.rect.left < 0:
+			self.rect.left = 0
+			self.x = self.rect.x
+			self.dirx = -self.dirx
+		if self.rect.right > SCREEN_WIDTH:
+			self.rect.right = SCREEN_WIDTH
+			self.x = self.rect.x
+			self.dirx = -self.dirx
+		
